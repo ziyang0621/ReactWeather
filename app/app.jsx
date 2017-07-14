7e60835059f14c7a2f2adab4392742de57980769
Ziyang tan <ziyang0621@gmail.com>
2017-07-13 20:56:22 -0700

    add node router

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Route, Router, IndexRoute, browserHistory} = require('react-router');
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
@@ -11,10 +11,10 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // App css
-require('style!css!sass!applicationStyles')
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Main}>
       <Route path="about" component={About}/>
       <Route path="examples" component={Examples}/>
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
 
 // Create our app
 var app = express();
+var router = express.Router();
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
@@ -12,7 +16,11 @@ app.use(function (req, res, next) {
   }
 });
 
-app.use(express.static('public'));
+router.get('/*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.use('/', router);
 
 app.listen(port, function () {
   console.log('Express sever is up on port ' + port);
